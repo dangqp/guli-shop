466e1e693f3593e753248697e624e96bce85e0f0
dangqp <d769246859@163.com>
2021-01-12 09:19:22 +0800

    修改for循环为java8 lamada写法

--- guli-parent/service/service-edu/src/main/java/com/dangqp/eduservice/service/impl/EduSubjectServiceImpl.java
+++ guli-parent/service/service-edu/src/main/java/com/dangqp/eduservice/service/impl/EduSubjectServiceImpl.java
@@ -48,36 +48,60 @@ public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubj
         //3 封装一级分类
         //查询出来所有的一级分类list集合遍历，得到每个一级分类对象，获取每个一级分类对象值，
         //封装到要求的list集合里面 List<OneSubject> finalSubjectList
-        for (int i = 0; i < oneSubjectList.size(); i++) { //遍历oneSubjectList集合
-            //得到oneSubjectList每个eduSubject对象
-            EduSubject eduSubject = oneSubjectList.get( i );
-            //把eduSubject里面值获取出来，放到OneSubject对象里面
+//        for (int i = 0; i < oneSubjectList.size(); i++) { //遍历oneSubjectList集合
+//            //得到oneSubjectList每个eduSubject对象
+//            EduSubject eduSubject = oneSubjectList.get( i );
+//            //把eduSubject里面值获取出来，放到OneSubject对象里面
+//            OneSubject oneSubject = new OneSubject();
+////            oneSubject.setId(eduSubject.getId());
+////            oneSubject.setTitle(eduSubject.getTitle());
+//            //eduSubject值复制到对应oneSubject对象里面
+//            BeanUtils.copyProperties( eduSubject, oneSubject );
+//            //多个OneSubject放到finalSubjectList里面
+//            finalSubjectList.add( oneSubject );
+//
+//            //在一级分类循环遍历查询所有的二级分类
+//            //创建list集合封装每个一级分类的二级分类
+//            List<TwoSubject> twoFinalSubjectList = new ArrayList<>();
+//            //遍历二级分类list集合
+//            for (int m = 0; m < twoSubjectList.size(); m++) {
+//                //获取每个二级分类
+//                EduSubject tSubject = twoSubjectList.get( m );
+//                //判断二级分类parentid和一级分类id是否一样
+//                if (tSubject.getParentId().equals( eduSubject.getId() )) {
+//                    //把tSubject值复制到TwoSubject里面，放到twoFinalSubjectList里面
+//                    TwoSubject twoSubject = new TwoSubject();
+//                    BeanUtils.copyProperties( tSubject, twoSubject );
+//                    twoFinalSubjectList.add( twoSubject );
+//                }
+//            }
+//            //把一级下面所有二级分类放到一级分类里面
+//            oneSubject.setChildren( twoFinalSubjectList );
+//        }
+        oneSubjectList.stream().forEach( eduSubject->{
             OneSubject oneSubject = new OneSubject();
 //            oneSubject.setId(eduSubject.getId());
 //            oneSubject.setTitle(eduSubject.getTitle());
             //eduSubject值复制到对应oneSubject对象里面
-            BeanUtils.copyProperties( eduSubject, oneSubject );
+            BeanUtils.copyProperties(eduSubject, oneSubject );
             //多个OneSubject放到finalSubjectList里面
             finalSubjectList.add( oneSubject );
 
             //在一级分类循环遍历查询所有的二级分类
             //创建list集合封装每个一级分类的二级分类
             List<TwoSubject> twoFinalSubjectList = new ArrayList<>();
-            //遍历二级分类list集合
-            for (int m = 0; m < twoSubjectList.size(); m++) {
-                //获取每个二级分类
-                EduSubject tSubject = twoSubjectList.get( m );
+            twoSubjectList.stream().forEach( towSubject->{
                 //判断二级分类parentid和一级分类id是否一样
-                if (tSubject.getParentId().equals( eduSubject.getId() )) {
+                if (towSubject.getParentId().equals( eduSubject.getId() )) {
                     //把tSubject值复制到TwoSubject里面，放到twoFinalSubjectList里面
                     TwoSubject twoSubject = new TwoSubject();
-                    BeanUtils.copyProperties( tSubject, twoSubject );
+                    BeanUtils.copyProperties( towSubject, twoSubject );
                     twoFinalSubjectList.add( twoSubject );
                 }
-            }
+            } );
             //把一级下面所有二级分类放到一级分类里面
             oneSubject.setChildren( twoFinalSubjectList );
-        }
+        });
         return finalSubjectList;
     }
 
